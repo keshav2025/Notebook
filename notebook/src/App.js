17b55d5fcd8b53d4f0f84b71f5d5b834876b4e85
keshav2025 <keshavgupta86036@gmail.com>
2024-03-17 18:57:58 +0530

    Initial commit

---
+++ notebook/src/App.js
@@ -0,0 +1,15 @@
+import Cards from './components/Cards';
+import './App.css';
+import Sidebar from './components/Sidebar';
+
+function App() {
+  return (
+    <div>
+      <Sidebar />
+      <Cards />
+      
+    </div>
+  );
+}
+
+export default App;
